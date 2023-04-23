bc68feed1e0ea68c64031386dc2f48cae38e7c0b
MarcusAfolabi <biormarcus@gmail.com>
2023-04-23 14:12:45 +0100

    reset password form

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,58 +1,55 @@
+@extends('layouts.connect')
+@section('title', 'Reset Password using the Request Link')
+@section('description', 'Enter a new password for your account. A simple but secure set of words that contains, capital, small letter, special character, and numbers, minimum of 8 character')
+@section('keywords', 'new password, account, reset, password, link, empowering, growing, networks, influencers, mentors')
+@section('canonical', 'https://oratoriogroup.org/reset-password')
+@section('content')
+<style>
+    body {
+        background-color: #f0f2f5;
+    }
+</style>
+<div class="lg:flex max-w-5xl min-h-screen mx-auto p-6 py-10">
+    <div class="flex flex-col items-center lg: lg:flex-row lg:space-x-10">
 
+        <div class="lg:mb-12 flex-1 lg:text-left text-center">
+            <img src="{{ asset('dark_oratorio_logo.png') }}" alt="oratorio group" class="lg:mx-0 lg:w-52 mx-auto w-40">
+        </div>
+        @if (session('status'))
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ session('status') }}
+        </div>
+        @endif
+        <x-validation-errors class="mb-1" />
 
-@extends('layouts.member')
-@section('title', 'Login to Oratorio Music Foundation')
-@section('main')
+        <div class="lg:mt-0 lg:w-96 md:w-1/2 sm:w-2/3 mt-10 w-full">
 
+            <form method="POST" action="{{ route('password.update') }}">
+                @csrf
 
-@if (session('status'))
-<div class="mb-4 font-medium text-sm text-green-600">
-    {{ session('status') }}
-</div>
-@endif
-<div class="bg-white">
-    <div class="container">
-        <div class="row justify-content-center align-items-center d-flex vh-100">
-            <div class="col-md-4 mx-auto">
-                <div class="osahan-login py-4">
-                    <div class="text-center mb-4">
-                        <a href="#"><img src="{{ asset('new_logo_oratorio_group.png') }}" alt="oratorio logo"></a>
-                        <h5 class="font-weight-bold mt-3">Welcome Back</h5>
-                        <p class="text-muted">Create your login detail and keep it safe.</p>
-                    </div>
-                    <x-validation-errors class="mb-4" />
+                <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-                        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-                        <div class="form-group">
-                            <label class="mb-1">Email</label>
-                            <div class="position-relative icon-form-control">
-                                <i class="feather-user position-absolute"></i>
-                                <x-input id="email" class="form-control" type="email" readonly name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="mb-1">Password</label>
-                            <div class="position-relative icon-form-control">
-                                <i class="feather-unlock position-absolute"></i>
-                                <x-input id="password" name="password" class="form-control" type="password" name="password" required autocomplete="new-password" />
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="mb-1">Confirm Password</label>
-                            <div class="position-relative icon-form-control">
-                                <i class="feather-unlock position-absolute"></i>
-                                <x-input id="password" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
-                            </div>
-                        </div>
-
-                        <button class="btn btn-danger btn-block text-uppercase" type="submit"> Proceed </button>
-
-                    </form>
+                <div class="block">
+                    <x-label for="email" value="{{ __('Email') }}" />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
                 </div>
-            </div>
+
+                <div class="mt-4">
+                    <x-label for="password" value="{{ __('Password') }}" />
+                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                    <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-button>
+                        {{ __('Reset Password') }}
+                    </x-button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
