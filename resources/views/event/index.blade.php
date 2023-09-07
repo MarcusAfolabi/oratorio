b7e6ef0a14efd7a3bb646f0a176909176d40a3df
MarcusAfolabi <biormarcus@gmail.com>
2023-09-07 09:11:00 +0100

    updated

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('title', 'Register for Annual Festive of Praise and Worship')
-@section('description', 'Register for Annual Festive of Praise and Worship. our community of inspired oratorio, vocal, singer, instrumentalist and more')
+@section('description', 'Register for Annual Festive of Praise and Worship. Our community of inspired oratorio, vocal, singer, instrumentalist and more')
 @section('keyword', 'about, about music, learn more about oratorio, know more about music, oratorio, oratorio group, choir, campus choir, joint choir, community, vocal, singer, instrumentalist and Musical group, Young talents, Live performances, Concerts, Music videos, Original compositions, Recording studio, Creative collaboration, Music industry,
 Entertaining performances, Dynamic artists, Music genres pop, rock, jazz, R&B, Music competitions, Fan base, Music education, Vocal coaching, Instrumental mastery, Music production, Sound engineering, Music promotion')
 @section('canonical', 'https://oratoriogroup.org/contact-oratorio')
