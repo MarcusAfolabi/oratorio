4007d1f87c45eb5f25f767ce01a1ad272bc3083b
MarcusAfolabi <biormarcus@gmail.com>
2024-03-03 20:29:58 +0100

    updated

--- app/Notifications/UserNotification.php
+++ app/Notifications/UserNotification.php
@@ -12,27 +12,19 @@ class UserNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
+     
     public function __construct(public User $user )
     {
         //
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
+    
     public function via(object $notifiable): array
     {
         return ['mail'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
+     
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
